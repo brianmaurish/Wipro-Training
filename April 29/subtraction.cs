dc83e7f8ed492bb30ab22a5477b9f1012c995a62
Brian Maurish J <brianmaurish03@gmail.com>
2025-04-29 15:25:59 +0530

    multiply

---
+++ April 29/subtraction.cs
@@ -0,0 +1,20 @@
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int num1=10;
+        int num2=2;
+        
+        Console.WriteLine("The subtraction is: "+ sub(num1,num2));
+    }
+    
+    static int sub(int a,int b)
+    {
+        return a-b;
+    }
+ 
+    
+}
