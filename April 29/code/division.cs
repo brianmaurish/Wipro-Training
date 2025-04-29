84eef5fb231e403798238a52551162b12c56cfd4
Brian Maurish J <brianmaurish03@gmail.com>
2025-04-29 15:40:55 +0530

    division

---
+++ April 29/code/division.cs
@@ -0,0 +1,20 @@
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int num1=10;
+        int num2=2;
+        
+        Console.WriteLine("The division is: "+ div(num1,num2));
+    }
+    
+    static int div(int a,int b)
+    {
+        return a/b;
+    }
+ 
+    
+}
