6d8f9152c3d25d22c9ae5ddbb3990514413c818e
Brian Maurish J <brianmaurish03@gmail.com>
2025-04-29 15:21:32 +0530

    helloworld code

---
+++ April 29/helloworld.cs
@@ -0,0 +1,25 @@
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("hello world");
+        Console.WriteLine("hi");
+        int age=25;
+        String name="Anny";
+        bool isstudent=true;
+        double fees= 25000.00;
+        
+        // Console.WriteLine("age is: "+age);
+        // Console.WriteLine("Name is: "+name);
+        // Console.WriteLine("is student: "+isstudent);
+        // Console.WriteLine("fees is: "+fees);
+        
+        Console.WriteLine($"age is:  {age}");
+        Console.WriteLine($"Name is: {name}");
+
+        
+    }
+}
