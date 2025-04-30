e45bad7e520da48c53b7c3425057608fdbc95db4
Brian Maurish J <brianmaurish03@gmail.com>
2025-04-30 09:42:08 +0530

    while loop

---
+++ April 30/code/whileloop.cs
@@ -0,0 +1,17 @@
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int i = 5;
+        while(i>=1)
+        {
+            Console.WriteLine(i);
+            i--;
+        }
+        
+    }
+    
+}
