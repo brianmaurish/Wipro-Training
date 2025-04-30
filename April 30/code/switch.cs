32d1bd6487788aa1b7e34194b0b2e3e90e7a7625
Brian Maurish J <brianmaurish03@gmail.com>
2025-04-30 09:22:36 +0530

    switch statement

---
+++ April 30/code/switch.cs
@@ -0,0 +1,37 @@
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int day=5;
+        
+        switch (day)
+        {
+            case 1:
+                Console.WriteLine("Sunday");
+                break;
+            case 2:
+                Console.WriteLine("Monday");
+                break;
+            case 3:
+                Console.WriteLine("Tuesday");
+                break;
+            case 4:
+                Console.WriteLine("Wednesday");
+                break;
+            case 5:
+                Console.WriteLine("Thursday");
+                break;
+            case 6:
+                Console.WriteLine("Friday");
+                break;
+            case 7:
+                Console.WriteLine("Saturday");
+                break;
+        }
+        
+    }
+    
+}
