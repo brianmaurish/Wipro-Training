e4dfb92dc583a8d40eeafb342419475a86679073
b84user22 <b84user22@rpsconsulting.in>
2025-05-06 15:02:13 +0530

    Count the number of words

---
+++ May 06/Exercise-1 CountNoOfWords.cs
@@ -0,0 +1,19 @@
+class Program
+{ 
+    public static void Main()
+    {
+        Console.Write("Enter a sentence: ");
+        string input = Console.ReadLine();
+
+        int wordCount = CountWords(input);
+        Console.WriteLine("Number of words: " + wordCount);
+    }
+    static int CountWords(string sentence)
+    {
+        string[] words = sentence.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+        return words.Length;
+    }
+}
+
+
+
