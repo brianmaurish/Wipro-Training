c073ed93454a9d635e7055228410de611e0bf3b6
b84user22 <b84user22@rpsconsulting.in>
2025-05-07 09:51:44 +0530

    Exercises commit

---
+++ May 07/Exercise 3 - FindDuplicateWordsInSentence.cs
@@ -0,0 +1,34 @@
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter a sentence: ");
+        String sentence=Console.ReadLine();
+        LongestWord(sentence);
+    }
+    static void LongestWord(String sentence)
+    {
+        string[] str = sentence.Split(' ');
+        bool[] visited=new bool[str.Length];
+        Console.WriteLine("The duplicate words are: ");
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (visited[i])
+                continue;
+            bool isDuplicate=false;
+            for(int j=i+1;j<str.Length; j++)
+            {
+                if (str[i] == str[j])
+                {
+                    visited[j] = true;
+                    isDuplicate = true;
+                    
+                }
+            }
+            if (isDuplicate)
+            {
+                Console.WriteLine(str[i]);
+            }
+        }
+    }
+}
