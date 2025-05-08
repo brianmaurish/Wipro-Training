ce14cef5e459f5d3bdd937670d8e8e9cd2d7e846
b84user22 <b84user22@rpsconsulting.in>
2025-05-08 16:40:11 +0530

    Integer operations ArrayList

--- May 08/Exercise 2 - IntegerOperationsArrayList.cs
+++ May 08/Exercise 2 - IntegerOperationsArrayList.cs
@@ -5,11 +5,17 @@ class Program
 {
     public static void Main()
     {
+        //Create a ArrayList
         ArrayList numbers = new ArrayList() { 5, 12, 34, 22, 45, 7, 19, 15, 27, 32 };
+
+        //Sort the ArrayList
         numbers.Sort();
+
+        //Find maximum and minimum numbers
         Console.WriteLine("The maximum value is: " + numbers[9]);
         Console.WriteLine("The minimun value is: " + numbers[0]);
 
+        //Find the average of numbers
         int sum = 0;
         foreach (int num in numbers)
         {
