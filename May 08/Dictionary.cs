797e7ef3984742d202abe0e378f4ef34616dccfd
b84user22 <b84user22@rpsconsulting.in>
2025-05-08 11:48:51 +0530

    Dictionary

---
+++ May 08/Dictionary.cs
@@ -0,0 +1,25 @@
+class Program
+{
+    public static void Main()
+    {
+        //Creating a Dictionary
+        Dictionary<int,string> students = new Dictionary<int, string>();
+
+        //Adding elements
+        students.Add(101, "Alice");
+        students.Add(102, "Bob");
+        students.Add(103, "Charlie");
+
+        //Accessing values using keys
+        Console.WriteLine("Student with ID 102: " + students[102]);
+
+        //Looping through the dictionary
+        foreach (var student in students)
+        {
+            {
+                Console.WriteLine($"ID: {student.Key}, Name: {student.Value}");
+
+            }
+        }
+    }
+}
