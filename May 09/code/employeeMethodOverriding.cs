173302a6caaa4c7a438996092ba3c440c6dcfda1
b84user22 <b84user22@rpsconsulting.in>
2025-05-09 15:48:02 +0530

    Method Overriding

---
+++ May 09/code/employeeMethodOverriding.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Employee
+    {
+       public virtual void Work()
+        {
+            Console.WriteLine("Employee is working");
+        }
+    }
+    class Manager : Employee
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Manager is supervising");
+        }
+    }
+}
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee emp = new Employee();
+            emp.Work();
+
+            Manager mgr = new Manager();
+            mgr.Work();
+
+            Employee polyEmp = new Manager();
+            polyEmp.Work();
+
+        }
+    }
+}
+
+
