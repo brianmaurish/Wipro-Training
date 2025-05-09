7757577aaeddbded47e729a16dabc3ac4196e275
b84user22 <b84user22@rpsconsulting.in>
2025-05-09 15:13:07 +0530

    Calculator Method Overloading

--- May 09/code/calculatorMethodOverloading.cs
+++ May 09/code/calculatorMethodOverloading.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp7
 {
     internal class Calculator
     {
+        //Addition 
         public int Add(int a,int b)
         {
             return a + b;
@@ -21,6 +22,38 @@ namespace ConsoleApp7
         public int Add(int a,int b,int c)
         {
             return a + b + c;
+        }
+
+        //Multiplication 
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Mul(double a, double b)
+        {
+            return a * b;
+        }
+
+        public int Mul(int a, int b, int c)
+        {
+            return a * b * c;
+        }
+
+        //Subtraction
+        public int Sub(int a,int b)
+        {
+            return a - b;
+        }
+
+        public double Sub(double a, double b)
+        {
+            return a - b;
+        }
+
+        public int Sub(int a, int b, int c)  
+        {
+            return a - b - c;
         }
     }
 }
@@ -36,6 +69,16 @@ namespace ConsoleApp7
             Console.WriteLine("Addition of 3 and 2 is: " + obj.Add(3, 2));
             Console.WriteLine("Addition of 3,5 and 2 is: " + obj.Add(3, 5, 2));
             Console.WriteLine("Addition of 3.2  and 2.5 is: " + obj.Add(3.2, 2.5));
+            Console.WriteLine();
+
+            Console.WriteLine("Multiplication of 3 and 2 is:  " + obj.Mul(3, 2));
+            Console.WriteLine("Multiplication of 3,5 and 2 is: " + obj.Mul(8, 2, 5));
+            Console.WriteLine("Multiplication of 3.2  and 2.5 is: " + obj.Mul(3.2, 2.5));
+            Console.WriteLine();
+
+            Console.WriteLine("Subtraction of 3 and 2 is:  "+ obj.Sub(3,2));
+            Console.WriteLine("Subtraction of 3,5 and 2 is: " + obj.Sub(8, 2, 5));
+            Console.WriteLine("Subtraction of 3.2  and 2.5 is: " + obj.Sub(3.2, 2.5));
 
         }
     }
