29d83b602073c4f385e40a75cce05896c89fe558
b84user22 <b84user22@rpsconsulting.in>
2025-05-12 09:12:31 +0530

    Interface

---
+++ May 12/code/interface.cs
@@ -0,0 +1,19 @@
+interface IShape
+{
+    void Draw();
+}
+class Circle : IShape
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing a circle");
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        Circle circle = new Circle();
+        circle.Draw();
+    }
+}
