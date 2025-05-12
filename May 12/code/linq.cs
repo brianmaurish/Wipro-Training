f739f958938d30a5b10f71a85ba7aaf4b34aaeaf
b84user22 <b84user22@rpsconsulting.in>
2025-05-12 17:04:39 +0530

    Find names by length using Linq

--- May 12/code/linq.cs
+++ May 12/code/linq.cs
@@ -7,14 +7,19 @@ class Program
     static void Main(string[] args)
     {
         string[] names = { "Anil", "Sharma", "Abdullah", "Imran", "Siva", "Naresh", "Suresh", "Suman" };
-        UsingLinq(names);
-    }
-    private static void UsingLinq(string[] names)
-    {
-        IEnumerable<string> query = from s in names
-                                    where s.Length ==5
-                                    orderby s
-                                    select s.ToUpper();
+        UsingLinqExtensions(names);
+}
+private static void UsingLinqExtensions(string[] names)
+{
+    /* IEnumerable<string> query = from s in names
+                                 where s.Length ==5
+                                 orderby s
+                                 select s.ToUpper();
+     */
+        IEnumerable<string> query = names
+                                 .Where(s => s.Length == 5)
+                                 .OrderBy(s => s)
+                                 .Select(s => s.ToUpper());
 
         foreach(string item in query)
         {
