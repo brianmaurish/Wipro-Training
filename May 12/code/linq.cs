b9c72fd3589c7f408521b7550aefcb01239c7b93
b84user22 <b84user22@rpsconsulting.in>
2025-05-12 17:25:41 +0530

    Linq syntax styles

--- May 12/code/linq.cs
+++ May 12/code/linq.cs
@@ -7,21 +7,52 @@ class Program
     static void Main(string[] args)
     {
         string[] names = { "Anil", "Sharma", "Abdullah", "Imran", "Siva", "Naresh", "Suresh", "Suman" };
-        UsingLinqExtensions(names);
-}
-private static void UsingLinqExtensions(string[] names)
-{
-    /* IEnumerable<string> query = from s in names
-                                 where s.Length ==5
-                                 orderby s
-                                 select s.ToUpper();
-     */
+        //UsingLinqQuery(names);
+        //UsingLinqExtensions(names);
+        UsingLinqFunctions(names);
+    }
+
+    /*private static void UsingLinqQuery(string[] names)
+    {
+        IEnumerable<string> query = from s in names
+                                     where s.Length ==5
+                                     orderby s
+                                     select s.ToUpper();
+
+
+        foreach (string item in query)
+        {
+            Console.WriteLine(item);
+        }
+    }*/
+
+    /*private static void UsingLinqExtensions(string[] names)
+    {
+
         IEnumerable<string> query = names
-                                 .Where(s => s.Length == 5)
-                                 .OrderBy(s => s)
-                                 .Select(s => s.ToUpper());
+                                     .Where(s => s.Length == 5)
+                                     .OrderBy(s => s)
+                                     .Select(s => s.ToUpper());
+                                      
 
         foreach(string item in query)
+        {
+            Console.WriteLine(item);
+        }
+    }*/
+
+    private static void UsingLinqFunctions(string[] names)
+    {
+        Func<string, bool> filter=s=>s.Length == 5;
+        Func<string, string> extract = s => s;
+        Func<string,string> project = s => s.ToUpper();
+
+        IEnumerable<string> query = names.Where(filter)
+                                            .OrderBy(extract)
+                                            .Select(project);
+
+
+        foreach (string item in query)
         {
             Console.WriteLine(item);
         }
