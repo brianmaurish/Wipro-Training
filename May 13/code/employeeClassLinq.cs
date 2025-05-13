042f8fcc5e02951469e02b935af65bb675f87771
b84user22 <b84user22@rpsconsulting.in>
2025-05-13 10:10:31 +0530

    Employee operations using Linq

--- May 13/code/employeeClassLinq.cs
+++ May 13/code/employeeClassLinq.cs
@@ -17,7 +17,7 @@ class Program
         Employee[] EmployeeArray = {
             new Employee() { EmployeeID = 1, EmployeeName = "John", Age = 18 },
             new Employee() { EmployeeID = 2, EmployeeName = "Steve", Age = 21 },
-            new Employee() { EmployeeID = 3, EmployeeName = "Bil", Age = 25 },
+            new Employee() { EmployeeID = 3, EmployeeName = "Bill", Age = 25 },
             new Employee() { EmployeeID = 4, EmployeeName = "Ram", Age = 20 },
             new Employee() { EmployeeID = 5, EmployeeName = "Ron", Age = 31 },
             new Employee() { EmployeeID = 6, EmployeeName = "Chris", Age = 17 },
@@ -42,12 +42,25 @@ class Program
         //Use LINQ to find teenager Employees
         Employee[] teenAgerEmployees = EmployeeArray.Where(s => s.Age > 12 && s.Age < 20).ToArray();
 
+        foreach(Employee emp in teenAgerEmployees)
+        {
+            Console.WriteLine(emp.EmployeeName);
+        }
+
         //Use LINQ to find first employee whose name is Bill
         Employee bill = EmployeeArray.Where(s=>s.EmployeeName=="Bill").FirstOrDefault();
+
+        if (bill != null)
+        {
+            Console.WriteLine("Found Bill: "+ bill.EmployeeName + ", Age: "+bill.Age);
+        }
 
         //Use LINQ to find employee whose EmployeeID is 5
         Employee Employee5 = EmployeeArray.Where(s => s.EmployeeID == 5).FirstOrDefault();
         
-
+        if(Employee5!=null)
+        {
+            Console.WriteLine("The employee ID 5: " + Employee5.EmployeeName);
+        }
     }
 }
