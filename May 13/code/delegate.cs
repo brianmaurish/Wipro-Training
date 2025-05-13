c7e71b5f86779e643a8b2d2ddf0fa933c68944ee
b84user22 <b84user22@rpsconsulting.in>
2025-05-13 10:29:14 +0530

    Delegate

---
+++ May 13/code/delegate.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+delegate void CustomDel(string s);
+class TestClass
+{
+    static void Hello(string s)
+    {
+        Console.WriteLine($"Hello, {s}!");
+    }
+    static void Main()
+    {
+        CustomDel hiDel;
+        hiDel = Hello;
+        Console.WriteLine("Invoking delegate hiDel: ");
+        hiDel("A");
+    }
+}
+
