376f4908fa3ab8638a9e6170c9dc57b3776660b5
b84user22 <b84user22@rpsconsulting.in>
2025-05-17 09:30:27 +0530

    Interface

---
+++ May 17/Assignment 1 - VehicleMaintenanceInterface.cs
@@ -0,0 +1,41 @@
+interface IMaintenance
+{
+    void Service();
+    void Repair();
+}
+class CarMaintenance : IMaintenance
+{
+    public void Service()
+    {
+        Console.WriteLine("Car is being serviced");
+    }
+    public void Repair()
+    {
+        Console.WriteLine("Car is being repaired");
+    }
+}
+class BikeMaintenance : IMaintenance
+{
+    public void Service()
+    {
+        Console.WriteLine("Bike is being serviced");
+    }
+    public void Repair()
+    {
+        Console.WriteLine("Bike is being repaired");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        IMaintenance car = new CarMaintenance();
+        car.Service();
+        car.Repair();
+
+        IMaintenance bike = new BikeMaintenance();
+        bike.Service();
+        bike.Repair();
+    }
+
+}
