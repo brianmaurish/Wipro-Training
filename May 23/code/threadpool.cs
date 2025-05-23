1b96d98d50bd6ec80958953592242abcf1dce0b4
b84user22 <b84user22@rpsconsulting.in>
2025-05-23 12:17:48 +0530

    Threadpool

---
+++ May 23/code/threadpool.cs
@@ -0,0 +1,19 @@
+class Program
+{
+    static void DoWork(object state)
+    {
+        Console.WriteLine($"ThreadPool Task Started - {state}");
+        Thread.Sleep(1000);
+        Console.WriteLine($"ThreadPool Task Finished - {state}");
+    }
+    static void Main()
+    {
+        for(int i=1;i<=5;i++)
+        {
+            ThreadPool.QueueUserWorkItem(DoWork,$"Task {i}");
+        }
+        Console.WriteLine("Main thread continues....");
+        Thread.Sleep(3000); //wait for background tasks to complete
+        Console.WriteLine("Main thread ends.");
+    }
+}
