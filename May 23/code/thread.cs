95269df0fbbd3c63ea66a2f656e3a3596b8c2e07
b84user22 <b84user22@rpsconsulting.in>
2025-05-23 09:39:07 +0530

    Threading

---
+++ May 23/code/thread.cs
@@ -0,0 +1,22 @@
+class Program
+{
+    static void PrintNumbers()
+    {
+        for(int i = 1;i <= 5;i++)
+        {
+            Console.WriteLine($"Thread: {i}");
+            Thread.Sleep(500);
+        }
+    }
+    static void Main()
+    {
+        Thread myThread = new Thread(PrintNumbers);
+        myThread.Start();
+
+        for(int i=1;i<=5;i++)
+        {
+            Console.WriteLine($"Main: {i}");
+            Thread.Sleep(500);
+        }
+    }
+}
