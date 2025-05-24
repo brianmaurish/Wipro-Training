c22f3eb51a13eedf823c4d16276e0cd65d760611
b84user22 <b84user22@rpsconsulting.in>
2025-05-24 11:35:21 +0530

    Async & Await tasks

---
+++ May 24/Exercise 4 - Async&Await.cs
@@ -0,0 +1,23 @@
+class Program
+{
+    static async Task DownloadFileAsync(string fileName, int delayMs)
+    {
+        Console.WriteLine($"Starting download: {fileName}");
+        await Task.Delay(delayMs);
+        Console.WriteLine($"Comnpleted download: {fileName} ({delayMs}ms)");
+    }
+
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Starting all downloads...\n");
+
+        Task file1 = DownloadFileAsync("file1.pdf", 3000);
+        Task file2 = DownloadFileAsync("file2.pdf", 2000);
+        Task file3 = DownloadFileAsync("file3.pdf", 4000);
+
+        await Task.WhenAll(file1, file2, file3);
+
+        Console.WriteLine("\nAll downloads completed");
+    }
+
+}
