f310d732a3a77a530620e215276d74cc825d6ba4
b84user22 <b84user22@rpsconsulting.in>
2025-05-29 13:00:54 +0530

    commit

---
+++ May 29/Employee Website/WebApplication11/WebApplication11/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication11.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
